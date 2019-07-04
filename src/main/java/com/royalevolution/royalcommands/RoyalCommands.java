912de1219a72c00ac8b6334365891621a68e956f
SSND <braeden2234@gmail.com>
2019-07-04 09:08:49 -0500

    Added main code and PlayerCommand

--- src/main/java/com/royalevolution/royalcommands/RoyalCommands.java
+++ src/main/java/com/royalevolution/royalcommands/RoyalCommands.java
@@ -1,13 +1,27 @@
 package com.royalevolution.royalcommands;
 
-/**
- * Hello world!
- *
- */
-public class RoyalCommands 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RoyalCommands extends JavaPlugin {
+
+	private static RoyalCommands instance;
+
+	@Override
+	public void onEnable() { // all necessary plugin startup functions go here
+
+	}
+
+	@Override
+	public void onDisable() {
+		instance = null;
+	}
+
+	public void reload() {
+		// TODO: make it so this function updates all mutable data and call it upon an 'rc reload' command
+	}
+
+	public static RoyalCommands getInstance() {
+		return instance;
+	}
+
 }
---
+++ src/main/java/com/royalevolution/royalcommands/commands/CommandHeal.java
@@ -0,0 +1,17 @@
+package com.royalevolution.royalcommands.commands;
+
+import org.bukkit.entity.Player;
+
+public class CommandHeal extends PlayerCommand {
+
+	public CommandHeal() {
+		super("heal"); // the command name
+	}
+
+	@Override
+	protected void run(Player sender, String[] args) {
+
+
+	}
+
+}
