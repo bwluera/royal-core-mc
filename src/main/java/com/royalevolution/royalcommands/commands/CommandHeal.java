38a44b4299719b288c05d72cc95f81def644228b
SSND <braeden2234@gmail.com>
2019-07-04 10:12:29 -0500

    Added heal command

--- src/main/java/com/royalevolution/royalcommands/commands/CommandHeal.java
+++ src/main/java/com/royalevolution/royalcommands/commands/CommandHeal.java
@@ -1,6 +1,11 @@
 package com.royalevolution.royalcommands.commands;
 
+import java.util.Collection;
+
+import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
+
+import com.royalevolution.royalcommands.RoyalCommands;
 
 public class CommandHeal extends PlayerCommand {
 
@@ -10,8 +15,29 @@ public class CommandHeal extends PlayerCommand {
 
 	@Override
 	protected void run(Player sender, String[] args) {
+		if (!sender.hasPermission("rc.heal"))
+			return;
+		else if (args.length == 0) // sender targets their self
+			sender.setHealth(sender.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
+		else if (args.length == 1) { // sender targets another player
+			final String playerName = args[0];
 
+			final Collection<? extends Player> onlinePlayers = RoyalCommands.getInstance().getServer().getOnlinePlayers();
+
+			for (final Player player : onlinePlayers)
+				if (player.getName().equals(playerName)) {
+					player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getDefaultValue());
+					break;
+				}
+
+		}
 
 	}
+
+
+	//@Override
+	//public List<String> tabComplete (CommandSender sender, String alias, String[] args) {
+	//	return super.tabComplete(sender, alias, args); // this will simply tab-complete with someone's username, the default
+	//}
 
 }
