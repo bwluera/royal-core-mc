f2f93d75cbb0638e8e03a28206a644c6807639d8
SSND <braeden2234@gmail.com>
2019-07-09 05:54:43 -0500

    Changed one character
    ow

--- src/main/java/com/royalevolution/royalcommands/CustomRecipes.java
+++ src/main/java/com/royalevolution/royalcommands/CustomRecipes.java
@@ -1,11 +1,14 @@
 package com.royalevolution.royalcommands;
 
+import com.royalevolution.royalcommands.utils.Common;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
 
 
 public class CustomRecipes {
@@ -18,24 +21,27 @@ public class CustomRecipes {
 		addRecipe(Material.EMERALD, Material.EMERALD, Material.EMERALD
 				, Material.EMERALD, Material.DIAMOND, Material.EMERALD
 				, Material.EMERALD, Material.EMERALD, Material.EMERALD
-				, helmet);
+				, helmet, "test_helmet");
 	}
 
 	private static void addRecipe (Material m1, Material m2, Material m3, 
 								   Material m4, Material m5, Material m6,
-								   Material m7, Material m8, Material m9, ItemStack outcome) {
-		NamespacedKey key = new NamespacedKey(RoyalCommands.getPlugin(), RoyalCommands.getPlugin().getDescription().getName());
+								   Material m7, Material m8, Material m9,
+								     ItemStack outcome, String outcomeID) {
+
+		NamespacedKey key = new NamespacedKey(RoyalCommands.getInstance(), outcomeID);
 		ShapedRecipe recipe = new ShapedRecipe(key, outcome);
 		recipe.shape("abc","def","ghi");
 		recipe.setIngredient('a', m1);
 		recipe.setIngredient('b', m2);
 		recipe.setIngredient('c', m3);
 		recipe.setIngredient('d', m4);
-		recipe.setIngredient('e', m4);
+		recipe.setIngredient('e', m5);
 		recipe.setIngredient('f', m6);
 		recipe.setIngredient('g', m7);
 		recipe.setIngredient('h', m8);
 		recipe.setIngredient('i', m9);
+
 		Bukkit.getServer().addRecipe(recipe);
 
 	}
