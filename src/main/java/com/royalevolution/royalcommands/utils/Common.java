f58f490df1c80466232a6f527d38e91bf71c22bb
einstein8612 <joey0312004@gmail.com>
2019-07-09 19:37:42 +0200

    Removed more random imports

--- src/main/java/com/royalevolution/royalcommands/utils/Common.java
+++ src/main/java/com/royalevolution/royalcommands/utils/Common.java
@@ -2,22 +2,19 @@ package com.royalevolution.royalcommands.utils;
 
 
 import java.lang.reflect.Field;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandMap;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.ShapedRecipe;
-import org.bukkit.permissions.PermissionAttachment;
 import org.bukkit.permissions.PermissionAttachmentInfo;
 import org.bukkit.plugin.PluginManager;
 
